f2e7afe227d472ec24fa11bdbd2db8b65f260c7e
LucasMikolaiewskiUGV <ens-lucasmikolaiewski@ugv.edu.br>
2024-05-13 20:14:49 -0300

    feat: Add sales visualizations and more

--- dashboard.py
+++ dashboard.py
@@ -79,3 +79,58 @@ else:
     ]
 
 category_df = filtered_df.groupby(by=["Category"], as_index=False)["Sales"].sum()
+
+
+
+with col1:
+    st.subheader("Vendas por categoria")
+    fig = px.bar(
+        category_df,
+        x="Category",
+        y="Sales",
+        text=["${:,.2f}".format(x) for x in category_df["Sales"]],
+        template="seaborn",
+    )
+    st.plotly_chart(fig, use_container_width=True, height=200)
+
+with col2:
+    st.subheader("Vendas por região")
+    fig = px.pie(filtered_df, values="Sales", names="Region", hole=0.5)
+    fig.update_traces(text=filtered_df["Region"])
+    st.plotly_chart(fig, use_container_width=True)
+
+cl1, cl2 = st.columns((2))
+
+with cl1:
+    with st.expander("Category_ViewData"):
+        st.write(category_df.style.background_gradient(cmap="Blues"))
+        csv = category_df.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            "Download data",
+            data=csv,
+            file_name="Category.csv",
+            mime="text/csv",
+            help="Clique aqui para baixar",
+        )
+
+with cl2:
+    with st.expander("Region_ViewData"):
+        region = filtered_df.groupby(by="Region", as_index=False)["Sales"].sum()
+        st.write(category_df.style.background_gradient(cmap="Oranges"))
+        csv = region.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            "Download data",
+            data=csv,
+            file_name="Region.csv",
+            mime="text/csv",
+            help="Clique aqui para baixar",
+        )
+
+filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+st.subheader("Analise")
+
+linechart = pd.DataFrame(
+    filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"]
+    .sum()
+    .reset_index()
+)
